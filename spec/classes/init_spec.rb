67c89c32427acaebacf549a1b2eb3eb6eb4c2720
Luther Barnum <luther.barnum@gmail.com>
2015-03-12 14:52:28 -0400

    changed name of module in spec_helper.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-describe 'issue_jabil' do
+describe 'etc_issue' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('issue_jabil') }
+    it { should contain_class('etc_issue') }
   end
 end
