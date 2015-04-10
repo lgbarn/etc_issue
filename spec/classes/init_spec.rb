4e392ec31e9cc465842af2a742dd33de15edd6ac
Luther Barnum <luther_barnum@jabil.com>
2015-04-10 12:30:01 -0400

    added spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
-describe 'etc_issue' do
 
+describe 'etc_issue' do
   context 'with defaults for all parameters' do
     it { should contain_class('etc_issue') }
+    it do
+      should contain_file('/etc/issue').with(
+        'ensure' => 'file',
+        'owner' => 'root',
+        'group' => 'root',
+        'mode' => '0644',
+        'source' => 'puppet:///modules/etc_issue/issue',
+    ) end
   end
+
 end
